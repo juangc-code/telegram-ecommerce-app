bffe9d3188a190a576c2d767d8a51737ba29f018
juangc-code <juan.gonzalez.collado@gmail.com>
2026-02-09 18:30:44 -0300

    Feature/subs stores (#22)
    * Add subscribed store cards
    
    * Add tma refresh everytime app is open on telegram

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import Router from "./routes/Router";
 import { AppProvider } from "./context/AppProvider";
 import { AuthProvider } from "./context/AuthProvider";
+import { useRawInitData } from '@tma.js/sdk-react';
 
 export default function App() {
+  const rawInitData = useRawInitData();
+
+  useEffect(() => {
+    // Refresh TMA initData whenever the app mounts/reopens
+    if (window.Telegram?.WebApp && rawInitData) {
+      localStorage.setItem('tma', rawInitData);
+      console.log('TMA initData refreshed');
+    }
+  }, [rawInitData]);
+
   return (
     <AuthProvider>
       <AppProvider>
